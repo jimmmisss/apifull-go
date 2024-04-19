3b86d67392e0a57ae1a3dc947da0b94f609fe6e5
Wesley Pereira <jimmmisss@gmail.com>
2024-04-19 16:25:33 -0300

    Creating handler products

---
+++ internal/infra/webserver/handlers/product_handler.go
@@ -0,0 +1,79 @@
+package handlers
+
+import (
+	"encoding/json"
+	"github.com.br/jimmmisss/api/internal/dto"
+	"github.com.br/jimmmisss/api/internal/entity"
+	"github.com.br/jimmmisss/api/internal/infra/database"
+	"net/http"
+	"strconv"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(productDB database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: productDB,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	p, errors := entity.NewProduct(product.Name, product.Price)
+	if errors != nil {
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: errors[0].Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
+	page := r.URL.Query().Get("page")
+	limit := r.URL.Query().Get("limit")
+	sort := r.URL.Query().Get("sort")
+
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		pageInt = 0
+	}
+
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		limitInt = 0
+	}
+
+	products, err := h.ProductDB.FindAll(pageInt, limitInt, sort)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(products)
+}
