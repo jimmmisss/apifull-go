f03ca8985757cde351c88b39739acaaefd7243b1
Wesley Pereira <jimmmisss@gmail.com>
2024-04-19 17:17:33 -0300

    Created handler products

--- internal/infra/webserver/handlers/product_handler.go
+++ internal/infra/webserver/handlers/product_handler.go
@@ -5,6 +5,7 @@ import (
 	"github.com.br/jimmmisss/api/internal/dto"
 	"github.com.br/jimmmisss/api/internal/entity"
 	"github.com.br/jimmmisss/api/internal/infra/database"
+	entityPkg "github.com.br/jimmmisss/api/pkg/entity"
 	"net/http"
 	"strconv"
 )
@@ -53,15 +54,14 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
-	page := r.URL.Query().Get("page")
-	limit := r.URL.Query().Get("limit")
-	sort := r.URL.Query().Get("sort")
+	page := r.PathValue("page")
+	limit := r.PathValue("limit")
+	sort := r.PathValue("sort")
 
 	pageInt, err := strconv.Atoi(page)
 	if err != nil {
 		pageInt = 0
 	}
-
 	limitInt, err := strconv.Atoi(limit)
 	if err != nil {
 		limitInt = 0
@@ -72,8 +72,73 @@ func (h *ProductHandler) GetProducts(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(products)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err = h.ProductDB.FindById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	err = h.ProductDB.Update(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *ProductHandler) DeleteProduct(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	_, err := h.ProductDB.FindById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+
+	}
+	err = h.ProductDB.Delete(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
