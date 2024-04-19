0bb555299d1b04521ee7e63dcaf1b4bec3ca8dff
Wesley Pereira <jimmmisss@gmail.com>
2024-04-19 17:31:49 -0300

    Execute main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"github.com.br/jimmmisss/api/configs"
+	"github.com.br/jimmmisss/api/internal/entity"
+	"github.com.br/jimmmisss/api/internal/infra/database"
+	"github.com.br/jimmmisss/api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&entity.Product{}, &entity.User{})
+	if err != nil {
+		panic(err)
+
+	}
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	routes := initializeRoutes(productHandler)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: routes,
+	}
+
+	log.Println("Server running on port 8080")
+	err = server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initializeRoutes(productHandler *handlers.ProductHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /products", productHandler.GetProducts)
+	mux.HandleFunc("GET /products/{id}", productHandler.GetProduct)
+	mux.HandleFunc("POST /products", productHandler.CreateProduct)
+	mux.HandleFunc("PUT /products/{id}", productHandler.UpdateProduct)
+	mux.HandleFunc("DELETE /products/{id}", productHandler.DeleteProduct)
+	return mux
 }
